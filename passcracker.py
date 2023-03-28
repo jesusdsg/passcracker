0e87492d9757ba0a164747837c8fd1d4a0dda710
Jesus Salcedo <jesalceg@gmail.com>
2023-03-28 17:22:54 -0500

    Improved the code to decrypt with md5

--- passcracker.py
+++ passcracker.py
@@ -1,17 +1,22 @@
 import hashlib
 
-welcome_message = """                                                                           *(*                       
+welcome_message = """
+
+*(*                       
 (((((((.                             .(((((((                             *(*                       
 ((.  ((( ,((((((   ((((((,  *((((((  (((      /((((((# .((((((/   ((((((. *(* /((/  ,(((((   (((((((
 ((. *(((      ((/ /(((     ,(((.    (((/      /((( /((      .((  (((*     *(%#((   (((  (((  ((((/#(
 ((((((.  ,((((((/  *(((((.  ,(((((( ((((      /((      ,#((((((  (((      *((((,   ((((((((  (((    
 ((.      ((/ (((/      (((      ,((, (((%. *( /((      (((  (((  (((*  ,  *(*/(((  (((/      (((    
-((.      ,((((/(/ (((((((. ,((((((*   ,(((((/ /((      .(((((((   ((((((. *(* .(((. (((((((  (((     """
+((.      ,((((/(/ (((((((. ,((((((*   ,(((((/ /((      .(((((((   ((((((. *(* .(((. (((((((  (((     
+
+
+"""
 
 print(welcome_message)
 found = 0
-hashed_password = input('Insert the hashed password')
-dict_directory = input('Insert the dictionary directory')
+hashed_password = input('Insert the hashed password: ')
+dict_directory = input('Insert the dictionary directory: ')
 
 try:
     dict_file = open(dict_directory, 'r')
@@ -20,7 +25,7 @@ except Exception:
 
 for word in dict_file:
     cipher_word = word.encode('utf-8')
-    hashed_word = hashlib.md5(cipher_word().strip())
+    hashed_word = hashlib.md5(cipher_word.strip())
     digest = hashed_word.hexdigest()
 
     if digest == hashed_password:
